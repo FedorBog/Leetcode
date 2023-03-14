1ab5beb17add11a60b701f91acf30d52a710f830
FedorBog <fedos89@gmail.com>
2023-03-14 17:49:14 +0500

    Two Sum

---
+++ Leetcode/src/TwoSum.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{3,3};
+        int target = 6;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        Map<Integer, Integer> numExist = new HashMap<>();
+
+        int missAdd = 0;
+
+        for (int i = 0; i < nums.length; i++){
+
+            missAdd = target - nums[i];
+
+            if ( numExist.containsKey(missAdd) ){
+                result[0] = numExist.get(missAdd);
+                result[1] = i;
+                break;
+            }
+            numExist.put(nums[i], i);
+        }
+        return result;
+    }
+
+}
